96a0e2d21dc437f50abe84644ff7a98b91916865
Artyom Osepyan <art@osepyan.ru>
2025-10-08 08:45:06 +0300

    chore: add test fixtures for testAddArrayPropertyItem

---
+++ tests/fixtures/PHPFileBuilderTest/original/class_with_array_properties.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RonasIT\Larabuilder\Tests\Support;
+
+use Some;
+
+class SomeClass
+{
+    public string $stringProperty = 'some value';
+
+    protected array $tags = ['one', 'two'];
+
+    public bool $notArray = false;
+
+    protected array $fillable = [
+        'name',
+        'email',
+    ];
+
+    public function __construct()
+    {
+    }
+
+    public function someMethod()
+    {
+    }
+}
---
+++ tests/fixtures/PHPFileBuilderTest/results/class_with_array_properties.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RonasIT\Larabuilder\Tests\Support;
+
+use Some;
+
+class SomeClass
+{
+    public string $stringProperty = 'some value';
+
+    protected array $tags = ['one', 'two', 'three'];
+
+    public bool $notArray = false;
+
+    protected array $fillable = [
+        'name',
+        'email',
+        'age'
+    ];
+
+    protected array $role = ['admin'];
+
+    public function __construct()
+    {
+    }
+
+    public function someMethod()
+    {
+    }
+}
