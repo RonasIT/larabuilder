4b74fb63d7dade00b07b76e4f57c78d7710bc215
DenTray <dpankratov@ronasit.com>
2026-03-31 11:42:10 +0600

    Apply suggestion from @DenTray

--- src/Enums/StatementAttributeEnum.php
+++ src/Enums/StatementAttributeEnum.php
@@ -2,12 +2,8 @@
 
 namespace RonasIT\Larabuilder\Enums;
 
-use RonasIT\Support\Traits\EnumTrait;
-
 enum StatementAttributeEnum: string
 {
-    use EnumTrait;
-
     case Parent = 'parent';
     case Previous = 'previous';
     case Comments = 'comments';
