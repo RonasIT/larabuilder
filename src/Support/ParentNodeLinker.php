a62cd9e37a0d4565924f6f8821bacffe1ca0a9e3
Artyom Osepyan <art@osepyan.ru>
2026-05-07 10:34:18 +0300

    fix: remarks

--- src/Support/ParentNodeLinker.php
+++ src/Support/ParentNodeLinker.php
@@ -2,6 +2,7 @@
 
 namespace RonasIT\Larabuilder\Support;
 
+use Illuminate\Support\Arr;
 use PhpParser\Node;
 use RonasIT\Larabuilder\Enums\StatementAttributeEnum;
 
@@ -10,17 +11,10 @@ class ParentNodeLinker
     public static function linkParents(Node $parent): void
     {
         foreach ($parent->getSubNodeNames() as $name) {
-            $child = $parent->$name;
-
-            if ($child instanceof Node) {
-                $child->setAttribute(StatementAttributeEnum::Parent->value, $parent);
-                static::linkParents($child);
-            } elseif (is_array($child)) {
-                foreach ($child as $item) {
-                    if ($item instanceof Node) {
-                        $item->setAttribute(StatementAttributeEnum::Parent->value, $parent);
-                        static::linkParents($item);
-                    }
+            foreach (Arr::wrap($parent->$name) as $child) {
+                if ($child instanceof Node) {
+                    $child->setAttribute(StatementAttributeEnum::Parent->value, $parent);
+                    static::linkParents($child);
                 }
             }
         }
