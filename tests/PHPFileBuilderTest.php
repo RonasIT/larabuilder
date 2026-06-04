1af2c8ff25d1ae4c9ac7b0dad06552a465468760
Anton Zabolotnikov <azabolotnikov@ronasit.com>
2026-06-04 11:55:45 +0500

    feat: ability remove method
    refs: https://github.com/RonasIT/larabuilder/issues/70

--- tests/PHPFileBuilderTest.php
+++ tests/PHPFileBuilderTest.php
@@ -690,7 +690,7 @@ class PHPFileBuilderTest extends TestCase
             [
                 'structure' => 'class.php',
                 'method' => 'save',
-                'result' => 'class_method_dont_removed.php',
+                'result' => 'class_method_wasnt_removed.php',
             ],
             [
                 'structure' => 'trait.php',
