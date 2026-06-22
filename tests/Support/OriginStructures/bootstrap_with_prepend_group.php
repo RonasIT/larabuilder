12b0c8329d50d94703a2f1f4ce8f2cd94303d19d
Anton Zabolotnikov <azabolotnikov@ronasit.com>
2026-06-22 12:17:56 +0500

    feat: add middlewares into prependToGroup
    refs: https://github.com/RonasIT/larabuilder/issues/74

--- tests/Support/OriginStructures/bootstrap_with_prepend_group.php
+++ tests/Support/OriginStructures/bootstrap_with_prepend_group.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\Middleware\Authenticate;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prependToGroup('api', ['throttle:60,10', Illuminate\Auth\Middleware\Authenticate::class]);
+        $middleware->prependToGroup('api', ['throttle:60,10', Authenticate::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- tests/fixtures/AppBootstrapBuilderTest/bootstrap_with_changed_prepend_group.php
+++ tests/fixtures/AppBootstrapBuilderTest/bootstrap_with_changed_prepend_group.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prependToGroup('api', ['throttle:60,10', Illuminate\Auth\Middleware\Authenticate::class]);
+        $middleware->prependToGroup('api', ['throttle:60,10', Authenticate::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
